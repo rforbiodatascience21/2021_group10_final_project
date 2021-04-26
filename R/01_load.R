dccce526f4a92734ad4de9aa3460170fefc4af26
lineroager <linero@dtu.dk>
2021-04-26 07:21:33 +0000

    Uploading data and loading excel sheets.

--- R/01_load.R
+++ R/01_load.R
@@ -3,7 +3,8 @@ rm(list = ls())
 
 
 # Load libraries ----------------------------------------------------------
-library("tidyverse")
+library(tidyverse)
+library(readxl)
 
 
 # Define functions --------------------------------------------------------
@@ -11,8 +12,22 @@ source(file = "R/99_project_functions.R")
 
 
 # Load data ---------------------------------------------------------------
-my_data_raw <- read_tsv(file = "data/_raw/my_raw_data.tsv")
+download.file(url = "https://doi.org/10.1371/journal.pone.0210064.s006",
+              destfile = "data/_raw/raw_sanctuary_data")
 
+# Loading all sheets into separate tables
+sanctuary_data_GI_behavior <- read_excel("data/_raw/raw_sanctuary_data",
+                                         sheet = "GI&Behavior")
+sanctuary_data_immune_microbiota <- read_excel("data/_raw/raw_sanctuary_data",
+                                               sheet = "Immune&Microbiota")
+sanctuary_data_fecal_metabolites <- read_excel("data/_raw/raw_sanctuary_data",
+                                               sheet = "FecalMetabolites")
+sanctuary_data_urine_metabolites <- read_excel("data/_raw/raw_sanctuary_data",
+                                               sheet = "UrineMetabolites")
+sanctuary_data_serum_metabolites <- read_excel("data/_raw/raw_sanctuary_data",
+                                               sheet = "SerumMetabolites")
+sanctuary_data_microbiota_ras <- read_excel("data/_raw/raw_sanctuary_data",
+                                            sheet = "MicrobiotaRAs")
 
 # Wrangle data ------------------------------------------------------------
 my_data <- my_data_raw # %>% ...
