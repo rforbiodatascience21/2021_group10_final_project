b7f472605f524d5a9cb98f16fee06ccc7b3f96c1
Rasdeh <rasdeh@dtu.dk>
2021-04-26 14:40:37 +0000

    Just changed mutate position

--- R/01_load.R
+++ R/01_load.R
@@ -35,9 +35,7 @@ sanctuary_data_microbiota_ras <- read_excel("data/_raw/raw_sanctuary_data",
 # Assigning metabolite origin and creating full joined metabolite table
 sanctuary_data_fecal_metabolites <- sanctuary_data_fecal_metabolites %>% 
   rename_at(vars(-Treatment, -Sample, -Baseline, -`Baseline Treatment`, -Mixer),
-            ~ paste0(.,"_fecal")) %>% 
-  mutate(Sample = tolower(Sample))
-  
+            ~ paste0(.,"_fecal")) 
 
 sanctuary_data_urine_metabolites <- sanctuary_data_urine_metabolites %>% 
   rename_at(vars(-(1:4)),
@@ -47,7 +45,8 @@ sanctuary_data_serum_metabolites <- sanctuary_data_serum_metabolites %>%
   rename_at(vars(-(1:22)),
             ~ paste0(.,"_serum"))
 
-sanctuary_data_metabolites <- sanctuary_data_fecal_metabolites %>% 
+sanctuary_data_metabolites <- sanctuary_data_fecal_metabolites %>%
+  mutate(Sample = tolower(Sample)) %>% 
   full_join(sanctuary_data_urine_metabolites,
             by = c("Sample", "Treatment", "Baseline", "Baseline Treatment")) %>% 
   full_join(sanctuary_data_serum_metabolites,
