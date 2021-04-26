f593a603c064dc1c189f3947f0e367880f8c2cad
Jana Alba Raguz <jana1raguz@gmail.com>
2021-04-26 02:54:39 +0200

    Add empty scripts

---
+++ R/04_analysis_i.R
@@ -0,0 +1,31 @@
+# Clear workspace ---------------------------------------------------------
+rm(list = ls())
+
+
+# Load libraries ----------------------------------------------------------
+library("tidyverse")
+
+
+# Define functions --------------------------------------------------------
+source(file = "R/99_project_functions.R")
+
+
+# Load data ---------------------------------------------------------------
+my_data_clean_aug <- read_tsv(file = "data/03_my_data_clean_aug.tsv")
+
+
+# Wrangle data ------------------------------------------------------------
+my_data_clean_aug %>% ...
+
+
+# Model data
+my_data_clean_aug %>% ...
+
+
+# Visualise data ----------------------------------------------------------
+my_data_clean_aug %>% ...
+
+
+# Write data --------------------------------------------------------------
+write_tsv(...)
+ggsave(...)
