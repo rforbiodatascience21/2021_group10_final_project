0a42b8f579913d0db3579dc228439742d2e46f56
lineroager <linero@dtu.dk>
2021-05-03 10:08:02 +0000

    Visualizing microbiome composition.

--- R/04_analysis_i.R
+++ R/04_analysis_i.R
@@ -4,6 +4,9 @@ rm(list = ls())
 
 # Load libraries ----------------------------------------------------------
 library(tidyverse)
+library(cowplot)
+library(ggpubr)
+library(RColorBrewer)
 
 
 # Define functions --------------------------------------------------------
@@ -24,19 +27,28 @@ my_data_clean_aug %>% ...
 
 # Visualize data ----------------------------------------------------------
 
+# Creating more colors for the microbiome plot
+mycolors <- colorRampPalette(brewer.pal(9, "Paired"))(34)
+
 # Playing with visualizing microbiome compositions in barplot
 final_data %>% 
   select(Sample, starts_with("k__")) %>% 
   filter(!str_detect(Sample, "^neg")) %>% 
   pivot_longer(cols = -Sample,
                names_to = "Taxa",
-               values_to = "Relative abundance") %>% 
+               values_to = "Relative_abundance") %>% 
+  separate(col = Taxa,
+           into = c("Kingdom", "Phylum", "Class_taxa", "Order_taxa", "Family", "Genus"),
+           sep = ";") %>% 
+  drop_na() %>% 
   ggplot(data = .,
          mapping = aes(x = Sample,
-                       y = `Relative abundance`,
-                       fill = Taxa))+
-  geom_col(show.legend = FALSE,
-           width = 0.9)
+                       y = Relative_abundance,
+                       fill = Class_taxa))+
+  geom_col()+
+  scale_fill_manual(values = mycolors,
+                    aesthetics = "fill")
+                     
 
 
 # Write data --------------------------------------------------------------
