a100cbdf63e73f7c9907309c6128136ef173cdf1
lineroager <linero@dtu.dk>
2021-05-03 14:52:41 +0000

    Cleaning up microbiome plot.

--- R/04_analysis_i.R
+++ R/04_analysis_i.R
@@ -4,9 +4,8 @@ rm(list = ls())
 
 # Load libraries ----------------------------------------------------------
 library(tidyverse)
-library(cowplot)
-library(ggpubr)
 library(RColorBrewer)
+library(haven)
 
 
 # Define functions --------------------------------------------------------
@@ -14,41 +13,52 @@ source(file = "R/99_project_functions.R")
 
 
 # Load data ---------------------------------------------------------------
-my_data_clean_aug <- read_tsv(file = "data/03_my_data_clean_aug.tsv")
+sanctuary_data <- read_tsv(file = "data/02_clean_data.tsv")
 
 
 # Wrangle data ------------------------------------------------------------
-my_data_clean_aug %>% ...
 
+# Subset data to pick out microbiome compositions and make
+# data long for plotting
 
-# Model data
-my_data_clean_aug %>% ...
-
-
-# Visualize data ----------------------------------------------------------
-
-# Creating more colors for the microbiome plot
-mycolors <- colorRampPalette(brewer.pal(9, "Paired"))(34)
-
-# Playing with visualizing microbiome compositions in barplot
-final_data %>% 
+microbiome_data = sanctuary_data %>% 
   select(Sample, starts_with("k__")) %>% 
-  filter(!str_detect(Sample, "^neg")) %>% 
   pivot_longer(cols = -Sample,
                names_to = "Taxa",
                values_to = "Relative_abundance") %>% 
+  mutate(Taxa = str_remove_all(Taxa,
+                               "[kpcofg]__"),
+         Taxa = str_remove_all(Taxa,
+                               "\\["),
+         Taxa = str_remove_all(Taxa,
+                                "\\]")) %>% 
   separate(col = Taxa,
            into = c("Kingdom", "Phylum", "Class_taxa", "Order_taxa", "Family", "Genus"),
            sep = ";") %>% 
   drop_na() %>% 
-  ggplot(data = .,
-         mapping = aes(x = Sample,
-                       y = Relative_abundance,
-                       fill = Class_taxa))+
+  mutate(Phylum = zap_empty(Phylum),
+         Class_taxa = zap_empty(Class_taxa),
+         Order_taxa = zap_empty(Order_taxa),
+         Family = zap_empty(Family),
+         Genus = zap_empty(Genus))
+
+
+# Visualize data ----------------------------------------------------------
+
+# Visualizing microbiome compositions in barplot
+
+ggplot(data = microbiome_data,
+       mapping = aes(x = Sample,
+                     y = Relative_abundance,
+                     fill = Order_taxa))+
   geom_col()+
-  scale_fill_manual(values = mycolors,
-                    aesthetics = "fill")
-                     
+  scale_fill_manual(values = rep(brewer.pal(11,
+                                            "Paired"),
+                                 times = 5),
+                    aesthetics = "fill")+
+  theme(axis.text.x = element_text(angle = 90,
+                                   vjust = 0.5,
+                                   hjust = 1))
 
 
 # Write data --------------------------------------------------------------
