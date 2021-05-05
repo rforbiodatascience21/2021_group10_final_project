8ba4655d19a95da3b938099cdd84f597be95f18d
lineroager <linero@dtu.dk>
2021-05-05 10:45:33 +0000

    PCA analysis performed in 05_analysis_ii.R

--- R/05_analysis_ii.R
+++ R/05_analysis_ii.R
@@ -4,28 +4,45 @@ rm(list = ls())
 
 # Load libraries ----------------------------------------------------------
 library(tidyverse)
+library(broom)
 
 # Define functions --------------------------------------------------------
 source(file = "R/99_project_functions.R")
 
 
 # Load data ---------------------------------------------------------------
-data <- read_tsv()
+sanctuary_data <- read_tsv(file = "data/02_clean_data.tsv")
 
 # Wrangle data ------------------------------------------------------------
 
-# Here Line will make a PCA analysis
+metabolite_data <- sanctuary_data %>% 
+  select(Sample,
+         Subject,
+         Treatment,
+         contains(c("_fecal", "_urine", "_serum"))) %>% 
+  filter(Subject != 208)
+
+# Filter out rows 17, 18, 19, and 32!
 
 # Augment data ------------------------------------------------------------
 
-
+pca_metabolites = metabolite_data %>% 
+  select(-Sample,
+         -Subject,
+         -Treatment) %>% 
+  scale() %>% 
+  na.omit() %>% 
+  prcomp()
 
 # Model data --------------------------------------------------------------
 
 
 # Visualize data ----------------------------------------------------------
 
-
+pca_metabolites %>%
+  augment(metabolite_data) %>% 
+  ggplot(aes(.fittedPC1, .fittedPC2, color = Treatment)) + 
+  geom_point(size = 1.5)
 
 # Write data --------------------------------------------------------------
 write_tsv()
