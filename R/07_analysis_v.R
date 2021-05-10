f7e78d15f8f1e3f88bd34d08e113b4b9817b5e05
barbarab23 <s200194@student.dtu.dk>
2021-05-10 12:43:29 +0200

    deleted excess data

--- R/07_analysis_v.R
+++ R/07_analysis_v.R
@@ -18,43 +18,31 @@ data <- read_tsv(file = "data/02_clean_data.tsv")
 Gut_metabolites_immune_cells_cytokines_data <- data %>% 
   select(Subject,
          Sample,
-         Timing,
-         Treatment,
-         Acetate_fecal,
          Butyrate_fecal,
          Propionate_fecal,
          Acetate_serum,
          Acetate_urine,
-         CD25_M,
-         CD25_PMA,
          IL10_CD4,
-         IFNg_CD4,
-         IFNg_CD8,
-         IL17_CD4,
-         TNFa_CD4,
          TNFa_CD8,
-         IL6_CD4, 
-         P_Actinobacteria,
-         G_Bifidobacterium,
-         contains("g__Akkermansia"),
-         G_Clostridium) %>% 
-  rename("Fecal acetate" = Acetate_fecal,
-         "Fecal butyrate" = Butyrate_fecal,
+         IL13_CD4)
+         
+  rename("Fecal butyrate" = Butyrate_fecal,
          "Serum acetate" = Acetate_serum,
          "Urine acetate" = Acetate_urine,
-         "Fecal proprionte" = Propionate_fecal,
-         "Actinobacteria" = P_Actinobacteria,
-         "Bifidobacterium" = G_Bifidobacterium,
-         "Clostridium" = G_Clostridium,
-         "Akkermansia" = `k__Bacteria;p__Verrucomicrobia;c__Verrucomicrobiae;o__Verrucomicrobiales;f__Verrucomicrobiaceae;g__Akkermansia`) %>% 
-  mutate(Treatment = case_when(Treatment == "Pre" ~ "Prebiotic",
-                               Treatment == "Syn" ~ "Synbiotic"))
+         "Fecal proprionte" = Propionate_fecal)
+  
+
+# Visualize data ----------------------------------------------------------
+# Scatterplot visualization of correlation between
+# certain gut metabolites and immune cells and cytokines
 
 
 
 
+           
+    
 
+ 
 
-
-
-
+  
+  
