5fce4371144d344e2f17eb527c45c59170b07e68
Rasdeh <rasdeh@dtu.dk>
2021-05-10 15:56:08 +0000

    analysis 10 run through

--- R/10_analysis_vi.R
+++ R/10_analysis_vi.R
@@ -12,13 +12,14 @@ source(file = "R/99_project_functions.R")
 
 # Load data ---------------------------------------------------------------
 sanctuary_data = read_tsv(file = "data/02_clean_data.tsv")
-immune_microbiota = read_tsv(file = "data/01_immune_microbiota.tsv")
 
 # Wrangle data ------------------------------------------------------------
 
 Immune_heatmap<- sanctuary_data %>% 
   filter(Subject != 208) %>%
-  unite(Subject_treatment_time, c(Subject, Treatment, Timing,), remove=FALSE) %>% 
+  unite(Subject_treatment_time, 
+        c(Subject, Treatment, 
+          Timing), remove=FALSE) %>% 
   select(Subject_treatment_time,
          contains(c("CD25",
                     "IL10_CD",
@@ -45,20 +46,19 @@ Immune_heatmap_plot <-Immune_heatmap %>%
   geom_tile()+
   labs(title = "Cell frequency before and after treatment",
        x = "Subject, time, and treatment",
-       y = "Immunce cell markers")+
+       y = "Immunce cell markers") +
   theme(axis.text.x = element_text(angle = 90, 
                                    vjust = 0.5),
         panel.background = element_blank(),
         axis.line = element_line(colour = "black"))
-Immune_heatmap_plot
 
 
 # Write data --------------------------------------------------------------
 
 write_tsv(x = Immune_heatmap,
-          file = "data/05_Immune_heatmap.tsv")
+          file = "data/10_Immune_heatmap.tsv")
 
-ggsave(filename = "05_Immune_heatmap_plot.png",
+ggsave(filename = "10_Immune_heatmap_plot.png",
        path = "results",
        plot = Immune_heatmap_plot,
        width = 12,
