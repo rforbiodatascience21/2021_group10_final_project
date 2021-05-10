9d2ae95aed6e122f625ac358a27ce2c86cccb54e
barbarab23 <s200194@student.dtu.dk>
2021-05-10 02:32:57 +0200

    starting code

--- R/07_analysis_v.R
+++ R/07_analysis_v.R
@@ -5,3 +5,11 @@ rm(list = ls())
 library(tidyverse)
 library(patchwork)
 
+# Define functions --------------------------------------------------------
+source(file = "R/99_project_functions.R")
+
+
+# Load data ---------------------------------------------------------------
+data <- read_tsv(file = "data/02_clean_data.tsv")
+
+
